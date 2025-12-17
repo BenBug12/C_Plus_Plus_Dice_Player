26519d0e7f12d4302b80e0c5edae5c8b0f7dbe22
BenBug12 <benjaminmarinjr64@gmail.com>
2025-12-17 19:45:52 +0000

    first update

---
+++ .ipynb_checkpoints/Basic_dice_main-checkpoint.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+//This is the first thing we are going to work on is just get a basic version running
+
+int main(){
+    string n;
+    cout << "Hello welcome to V1 of the program" << endl;       //Temporary test feature
+    cin >> n ;
+    cout << "It is " << n << " you've entered" << endl;
+    return 0;
+}
